fd036460bc0564f16ed3c4d5462148846336e627
Diogo Correia <diogocorreia1996@gmail.com>
2024-09-20 18:31:13 +0100

    SearchApiSdk implementation using RestClient
    Implement searchBooks method using query parameters

---
+++ openlibrary-api-sdk/src/main/java/com/dmsc/openlibraryapi/sdk/RestClientSearchApiSdk.java
@@ -0,0 +1,36 @@
+package com.dmsc.openlibraryapi.sdk;
+
+import com.dmsc.openlibraryapi.model.SearchSdkRequest;
+import com.dmsc.openlibraryapi.model.SearchSdkResponse;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.util.UriBuilder;
+
+import java.util.Optional;
+
+public class RestClientSearchApiSdk implements SearchApiSdk {
+
+    private final RestClient restclient;
+
+    public RestClientSearchApiSdk(RestClient.Builder restClient) {
+        this.restclient = restClient.build();
+    }
+
+    @Override
+    public SearchSdkResponse searchBooks(SearchSdkRequest request) {
+        RestClient.RequestHeadersSpec<?> uri = this.restclient.get()
+            .uri(uriBuilder -> {
+                    UriBuilder builder = uriBuilder
+                        .path("search.json")
+                        .queryParam("title", request.getTitle());
+                    Optional.ofNullable(request.getOffset())
+                        .ifPresent(offset -> builder.queryParam("offset", offset));
+                    Optional.ofNullable(request.getPage())
+                        .ifPresent(page -> builder.queryParam("page", page));
+                    return builder.build();
+                }
+            );
+        return uri
+            .retrieve()
+            .body(SearchSdkResponse.class);
+    }
+}
