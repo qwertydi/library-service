068999f7bf8d89972be6960cf0b6b4b81deff3b5
Diogo Correia <diogocorreia1996@gmail.com>
2024-09-20 16:50:09 +0100

    Add Library Service API Spring Boot microservice

---
+++ library-service-api/src/main/java/com/dmsc/libraryserviceapi/LibraryServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.dmsc.libraryserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryServiceApiApplication.class, args);
+    }
+
+}
---
+++ library-service-api/src/test/java/com/dmsc/libraryserviceapi/LibraryServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmsc.libraryserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
