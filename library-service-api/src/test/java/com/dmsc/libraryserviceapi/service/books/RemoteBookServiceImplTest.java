f8dfbf1b49a2f554d227110f9b260bead7773233
Diogo Correia <diogocorreia1996@gmail.com>
2024-09-24 15:26:19 +0100

    Implement search books by title using remote service

---
+++ library-service-api/src/test/java/com/dmsc/libraryserviceapi/service/books/RemoteBookServiceImplTest.java
@@ -0,0 +1,86 @@
+package com.dmsc.libraryserviceapi.service.books;
+
+import com.dmsc.libraryserviceapi.model.book.BookResponse;
+import com.dmsc.openlibraryapi.model.BookSdk;
+import com.dmsc.openlibraryapi.model.SearchSdkRequest;
+import com.dmsc.openlibraryapi.model.SearchSdkResponse;
+import com.dmsc.openlibraryapi.sdk.SearchApiSdk;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class RemoteBookServiceImplTest {
+
+    private RemoteBookService classUnderTest;
+    private SearchApiSdk mockSearchApiSdk;
+
+    @BeforeEach
+    void beforeEach() {
+        mockSearchApiSdk = mock(SearchApiSdk.class);
+        classUnderTest = new RemoteBookServiceImpl(mockSearchApiSdk);
+    }
+
+    @Test
+    void testGetById() {
+        assertFalse(classUnderTest.searchBookById("id").isPresent());
+    }
+
+    @Nested
+    class SearchByTitle {
+        @Test
+        void testResults() {
+            /* Preparation */
+            SearchSdkRequest request = SearchSdkRequest.builder()
+                .title("title")
+                .build();
+
+            BookSdk bookSdk = new BookSdk();
+            bookSdk.setTitle("title");
+            SearchSdkResponse sdkResponse = new SearchSdkResponse();
+            sdkResponse.setDocs(Collections.singletonList(bookSdk));
+
+            when(mockSearchApiSdk.searchBooks(request)).thenReturn(sdkResponse);
+
+            /* Execution */
+            List<BookResponse> responses = classUnderTest.searchBookByTitle("title");
+
+            /* Verification */
+            assertNotNull(responses);
+            assertEquals(1, responses.size());
+            assertEquals("title", responses.get(0).getTitle());
+            verify(mockSearchApiSdk).searchBooks(request);
+        }
+
+        @Test
+        void testNoResults() {
+            /* Preparation */
+            SearchSdkRequest request = SearchSdkRequest.builder()
+                .title("title")
+                .build();
+
+            SearchSdkResponse sdkResponse = new SearchSdkResponse();
+            sdkResponse.setDocs(new ArrayList<>());
+
+            when(mockSearchApiSdk.searchBooks(request)).thenReturn(sdkResponse);
+
+            /* Execution */
+            List<BookResponse> responses = classUnderTest.searchBookByTitle("title");
+
+            /* Verification */
+            assertNotNull(responses);
+            assertEquals(0, responses.size());
+            verify(mockSearchApiSdk).searchBooks(request);
+        }
+    }
+}
