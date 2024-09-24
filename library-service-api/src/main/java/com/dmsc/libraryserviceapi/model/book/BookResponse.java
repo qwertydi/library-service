1b221f12ded9a30d8da696e7791bf1a7ca5ec3bf
Diogo Correia <diogocorreia1996@gmail.com>
2024-09-24 14:00:19 +0100

    Add factory method to create BookResponse from entity

--- library-service-api/src/main/java/com/dmsc/libraryserviceapi/model/book/BookResponse.java
+++ library-service-api/src/main/java/com/dmsc/libraryserviceapi/model/book/BookResponse.java
@@ -1,5 +1,9 @@
 package com.dmsc.libraryserviceapi.model.book;
 
+import com.dmsc.libraryserviceapi.entity.AuthorEntity;
+import com.dmsc.libraryserviceapi.entity.LanguagesEntity;
+import com.dmsc.libraryserviceapi.entity.LocalBookEntity;
+import com.dmsc.libraryserviceapi.util.IdGeneratorUtil;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,4 +23,14 @@ public class BookResponse {
     private List<String> authors;
     private Integer publishYear;
     private List<String> languages;
+
+    public static BookResponse fromLocalBook(LocalBookEntity localBookEntity) {
+        return BookResponse.builder()
+            .authors(localBookEntity.getAuthors().stream().map(AuthorEntity::getName).toList())
+            .languages(localBookEntity.getLanguages().stream().map(LanguagesEntity::getLanguage).toList())
+            .publishYear(localBookEntity.getPublishYear())
+            .id(IdGeneratorUtil.build(BookSystemEnum.LOCAL, String.valueOf(localBookEntity.getId())))
+            .title(localBookEntity.getTitle())
+            .build();
+    }
 }
