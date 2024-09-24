57f57365978118abe2072ca60a4777bbfe771deb
Diogo Correia <diogocorreia1996@gmail.com>
2024-09-24 14:00:19 +0100

    Add factory method to create BookResponse from entity

--- library-service-api/src/main/java/com/dmsc/libraryserviceapi/model/book/BookResponse.java
+++ library-service-api/src/main/java/com/dmsc/libraryserviceapi/model/book/BookResponse.java
@@ -1,5 +1,9 @@
 package com.dmsc.libraryserviceapi.model.book;
 
+import com.dmsc.libraryserviceapi.entity.AuthorEntity;
+import com.dmsc.libraryserviceapi.entity.LanguagesEntity;
+import com.dmsc.libraryserviceapi.entity.LocalBookEntity;
+import com.dmsc.libraryserviceapi.util.IdGeneratorUtil;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -7,6 +11,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.Objects;
 
 @AllArgsConstructor
 @Builder
@@ -19,4 +24,28 @@ public class BookResponse {
     private List<String> authors;
     private Integer publishYear;
     private List<String> languages;
+
+    public static BookResponse fromLocalBook(LocalBookEntity localBookEntity) {
+        if (Objects.isNull(localBookEntity)) {
+            throw new IllegalStateException("Illegal Entity");
+        }
+
+        List<String> listAuthors = localBookEntity.getAuthors()
+            .stream()
+            .map(AuthorEntity::getName)
+            .toList();
+
+        List<String> listLanguages = localBookEntity.getLanguages()
+            .stream()
+            .map(LanguagesEntity::getLanguage)
+            .toList();
+
+        return BookResponse.builder()
+            .authors(listAuthors)
+            .languages(listLanguages)
+            .publishYear(localBookEntity.getPublishYear())
+            .id(IdGeneratorUtil.build(BookSystemEnum.LOCAL, String.valueOf(localBookEntity.getId())))
+            .title(localBookEntity.getTitle())
+            .build();
+    }
 }
