3f2571481fba45635a2e91a53d278a45c4f4f54b
Diogo Correia <diogocorreia1996@gmail.com>
2024-09-24 11:31:52 +0100

    Create interfaces for Book Management
    CommonBookServices - common operations shared between other interfaces
    BookManagementService - operations related to the books
    LocalBookService - Book operations using local parties
    RemoteBookService - Book operations using third party

---
+++ library-service-api/src/main/java/com/dmsc/libraryserviceapi/service/books/RemoteBookService.java
@@ -0,0 +1,8 @@
+package com.dmsc.libraryserviceapi.service.books;
+
+/**
+ * Interface for managing remove book operations, extending common book services.
+ * The implementation will contain the Third Party connection.
+ */
+public interface RemoteBookService extends CommonBookServices {
+}
